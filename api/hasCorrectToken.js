7736b28b923cd28cd66e9d7fefb5147571ed6862
Milesq <wmilosz88@gmail.com>
2020-12-02 18:19:23 +0100

    refactor: move finding user to FP

--- api/hasCorrectToken.js
+++ api/hasCorrectToken.js
@@ -1,7 +1,9 @@
+const R = require('ramda')
+
 module.exports = tokens => (req, res, next) => {
   const { token } = req.params
 
-  const user = tokens.find(({ token: sysToken }) => sysToken === token)
+  const user = R.find(R.pipe(R.prop('token'), R.equals(token)), tokens)
 
   if (!user) {
     return res.sendStatus(401)
